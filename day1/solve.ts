20e48c3f94b38fb8de1c6a94e1fd9b80c8b98777
JM <jmjanzen@gmail.com>
2024-12-02 11:43:16 -0500

    fix: add day 1 part 1 solution
    Brute force, but at least it's not O(n^2).

--- day1/solve.ts
+++ day1/solve.ts
@@ -3,13 +3,27 @@ import { readFileSync } from 'fs'
 const f = readFileSync('./input.txt', 'utf-8')
 const lines = f.split("\n")
 
-let foo = []
-let bar = []
+let rights = {}
 for (const i in lines) {
 	if (lines[i] == '') { continue }
 
-	const [left, right] = lines[i].split(/\s+/)
+	const [, right] = lines[i].split(/\s+/)
+	if (rights[right] === undefined) {
+		rights[right] = 0
+	}
 
-	foo.push(Number(left))
-	bar.push(Number(right))
+	rights[right]++
 }
+
+let ans = 0
+for (const i in lines) {
+	const [left, right] = lines[i].split(/\s+/)
+	const similarity = rights[left]
+
+	if (similarity) {
+		console.log(`${left} appears ${rights[left]} times in right list`)
+		ans += (Number(left) * rights[left])
+	}
+}
+
+console.dir(ans)
