07f4cf1c5efd5a4d0c2a9b6da89f4db8cdc47231
JM <jmjanzen@gmail.com>
2024-12-02 10:56:55 -0500

    fix: don't need to sort
    We're going to hash the input and get that delicioso O(1) search time.

--- day1/solve.ts
+++ day1/solve.ts
@@ -13,18 +13,3 @@ for (const i in lines) {
 	foo.push(Number(left))
 	bar.push(Number(right))
 }
-
-foo = foo.sort()
-bar = bar.sort()
-
-console.log(foo[0], bar[0])
-
-let sum: number = 0
-for (const i in foo) {
-	const diff = Math.abs(foo[i] - bar[i])
-	//console.log(`The diff between ${foo[i]} and ${bar[i]} is ${diff}`)
-
-	sum += diff
-}
-
-console.dir(sum)
