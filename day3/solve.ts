6086abd813891b54f2fd39acbfa66e48c952973f
JM <jmjanzen@gmail.com>
2024-12-23 13:18:11 -0500

    fix: add day 3 part 2 solution

--- day3/solve.ts
+++ day3/solve.ts
@@ -5,9 +5,17 @@ const input = readFileSync('./input.txt', 'utf-8')
 
 const output = ((input: string): number => {
     let answer = 0
-    const muls = input.match(/mul\(\d+,\d+\)/g)
+    let active = true
+    const muls = input.match(/(do\(\)|don't\(\)|mul\(\d+,\d+\))/g)
     for (const mul of muls!) {
-        const op = mul.match(/[a-z]+/)![0]
+        const op = mul.match(/[a-z']+/)![0]
+        if (op.startsWith('do')) {
+            if (op === 'do') active = true
+            if (op === "don't") active = false
+            continue
+        }
+        if (!active) continue
+
         const [a, b] = mul.match(/\d+/g)!
 
         if (op === 'mul') {
